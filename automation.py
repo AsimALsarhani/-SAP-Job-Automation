b3e9b7d831a362a11b020e36368be7948b1b80a9
AsimALsarhani <asimalsarhani@gmail.com>
2025-03-22 03:28:09 +0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -18,7 +18,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # EMAIL SETTINGS
 SENDER_EMAIL = "Mshtag1990@gmail.com"
 RECEIVER_EMAIL = "Asimalsarhani@gmail.com"
-EMAIL_PASSWORD = "ufkd knzp bqyb gjyg"  # Replace with your Gmail App Password
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # Use environment variable for security
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 
@@ -52,6 +52,7 @@ def automate_process():
             EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Sign In')]"))
         )
         sign_in_button.click()
+        time.sleep(5)
 
         # Scroll to the bottom of the page
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -60,12 +61,4 @@ def automate_process():
         # Locate the Save button
         save_button_xpath = "/html/body/as:ajaxinclude/as:ajaxinclude/div[2]/div[2]/div/form/div[3]/div[2]/div[1]/div[23]/div/span"
         save_button = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, save_button_xpath))
-        )
-
-        # Ensure it's visible
-        driver.execute_script("arguments[0].scrollIntoView(true);", save_button)
-        time.sleep(2)
-
-        # Screenshot before clicking Save
-        before_cli
+            EC.presence_of_e
